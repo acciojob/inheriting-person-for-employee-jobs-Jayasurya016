971bcd254e9d70d3255a830396a4b2a0e2886688
JAYASURYA KADAKUZHILL JOB <161327970+Jayasurya016@users.noreply.github.com>
2025-09-01 13:59:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,37 +1,36 @@
-// complete this js code
-
-	class Person{
-    constructor(name, age){
-        this.name = name;
-        this.age = age;
+function PersonClass() {
+  class Person {
+    constructor(name, age) {
+      this.name = name;
+      this.age = age;
     }
-    greet(){
-        console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+    greet() {
+      console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
     }
+  }
+  return Person;
 }
 
-	class Employee extends Person{
-    constructor(name,age,jobTitle){
-        super(name,age)
-        this.jobTitle = jobTitle
+function EmployeeClass(Person) {
+  class Employee extends Person {
+    constructor(name, age, jobTitle) {
+      super(name, age);
+      this.jobTitle = jobTitle;
     }
-jobGreet(){
-    console.log(`Hello, my name is ${this.name}, I am <age> years old, and my job title is ${this.jobTitle}`)
+    jobGreet() {
+      console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
+    }
+  }
+  return Employee;
 }
 
-}
+// Get classes from functions
+const Person = PersonClass();
+const Employee = EmployeeClass(Person);
 
-
-// Creating an instance of Person
+// Test
 const person = new Person("Alice", 25);
 person.greet();
-// Expected Output: Hello, my name is Alice, I am 25 years old.
 
-// Creating an instance of Employee
 const employee = new Employee("Bob", 30, "Manager");
 employee.jobGreet();
-
-// Expected Output: Hello, my name is Bob, I am 30 years old, and my job title is Manager.
-// Do not change code below this line
-window.Person = Person;
-window.Employee = Employee;
